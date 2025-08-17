88f831833f704d8abaac280e7538e3b113b13288
EladEk-Amdocs <elade@amdocs.com>
2025-08-17 17:07:36 +0300

    c

--- src/types/react-router-v7-flags.d.ts
+++ src/types/react-router-v7-flags.d.ts
@@ -1,21 +1,8 @@
-// Augment types so TS accepts v7 future flags even if your installed version predates them.
 declare module 'react-router' {
   interface FutureConfig {
     v7_startTransition?: boolean;
     v7_relativeSplatPath?: boolean;
     v7_fetcherPersist?: boolean;
     v7_normalizeFormMethod?: boolean;
-  }
-}
-
-declare module 'react-router-dom' {
-  // Some versions also allow passing future flags to RouterProvider; we add it for safety.
-  interface RouterProviderProps {
-    future?: {
-      v7_startTransition?: boolean;
-      v7_relativeSplatPath?: boolean;
-      v7_fetcherPersist?: boolean;
-      v7_normalizeFormMethod?: boolean;
-    };
   }
 }
